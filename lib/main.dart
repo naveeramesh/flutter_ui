af7501b63f43f9c9c21d54e7beac468cf1d7e31e
navee <naveenarameshvardhini@gmail.com>
2021-12-02 17:32:49 +0530

    fix/Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/assistant.dart';
-import 'package:flutter_ui/hiddendrawer.dart';
-import 'package:flutter_ui/swipecard.dart';
-import 'package:flutter_ui/login.dart';
 import 'package:flutter_ui/tiktok/tiktokui.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Flutter UI',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
