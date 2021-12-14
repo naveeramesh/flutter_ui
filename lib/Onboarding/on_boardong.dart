44997f8ccf399ac5bed25bd061196b4316ef0d24
navee <naveenarameshvardhini@gmail.com>
2021-12-14 20:16:12 +0530

    feat:Onbaording base

---
+++ lib/Onboarding/on_boardong.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({Key key}) : super(key: key);
+
+  @override
+  _OnBoardingState createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
